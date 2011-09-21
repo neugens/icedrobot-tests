05ee2a1b506417f38ae574dc687bf4151a1f4ed4
Roman Kennke <rkennke@gmail.com>
2011-09-21 21:52:01 +0200

    Initialize AppManager.

--- src/main/java/android/app/BluetoothTest.java
+++ src/main/java/android/app/BluetoothTest.java
@@ -1,10 +1,13 @@
 package android.app;
 
+import org.icedrobot.app.SimpleAppManager;
+
 import android.bluetooth.BluetoothAdapter;
 
 public class BluetoothTest {
 
     public static void main(String [] args) {
+        SimpleAppManager.init();
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         String toast;
         if (adapter.isEnabled()) {
