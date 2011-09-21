0e1a004de0d9821858ca6e72311d809ad47fad95
Mario Torre <neugens.limasoftware@gmail.com>
2011-09-21 21:17:23 +0200

    New simple test.

--- src/main/java/android/app/BluetoothTest.java
+++ src/main/java/android/app/BluetoothTest.java
@@ -1,8 +1,19 @@
 package android.app;
 
+import android.bluetooth.BluetoothAdapter;
+
 public class BluetoothTest {
 
     public static void main(String [] args) {
-        
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        String toast;
+        if (adapter.isEnabled()) {
+            String address = adapter.getAddress();
+            String name = adapter.getName();
+            toast = name + " : " + address;
+        } else {
+            toast = "fluff not enabled";
+        }
+        System.err.println(toast);
     }
 }
